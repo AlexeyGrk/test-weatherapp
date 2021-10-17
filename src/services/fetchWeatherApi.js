cd09ad1b7d86ec002785e8e80dbd0a61572debab
AlexeyGrk <alexgmirko@gmail.com>
2021-10-18 00:43:45 +0300

    add firt draft weather-app

--- src/services/fetchWeatherApi.js
+++ src/services/fetchWeatherApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export async function getCurrentWeather(current) {
   try {
     const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${current.latitude}&lon=${current.longitude}&appid=27e7b52884ad4061c14490af505012c4&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${current.latitude}&lon=${current.longitude}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
 
       //`https://api.openweathermap.org/data/2.5/weather?lat=69.3&lon=139.9&appid=dbee7d5fa08c5b02343c384ae09d6fa7&units=metric`
       // [69.3, 139.9]- deputatsky
@@ -20,7 +20,7 @@ export async function getCurrentWeather(current) {
 export async function getWeatherByCity(cityName) {
   try {
     const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=27e7b52884ad4061c14490af505012c4&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
     );
 
     const data = response.data;
